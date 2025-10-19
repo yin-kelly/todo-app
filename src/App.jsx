63942886f2f51bb6a5d67abcdbacf1f64d458ad6
kelly_yin <ky132@duke.edu>
2025-10-19 17:43:24 -0400

    set up project with basic layout and CSS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="container">
+        <header className="header">
+          <h1>My Todo List</h1>
+          <p>Stay organized and productive</p>
+        </header>
+      </div>
+    </div>
+  );
+}
